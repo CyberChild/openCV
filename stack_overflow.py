928fd78887cce844c4e2ceb4bff6ca8678d92913
CyberChild <darkpaladinknight@gmail.com>
2014-05-30 11:14:48 +0530

    Python urllib.py problem

---
+++ stack_overflow.py
@@ -0,0 +1,16 @@
+I tried that but the response I get is always of the same page....This is the code that I am using is
+import urllib.parse
+import urllib.request
+
+url = 'http://59.145.203.105/'
+values = {'txtRoll' : '1305024',
+          'txtEnroll' : '13139428723',
+          'btnSubmit' : 'formSubmit'
+           }
+
+data = urllib.parse.urlencode(values)
+binary_data=data.encode('ascii')
+req = urllib.request.Request(url, binary_data)
+response = urllib.request.urlopen(req)
+the_page = response.read()
+print(the_page)
