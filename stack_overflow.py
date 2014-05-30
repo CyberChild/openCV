e730bdd67b255dbd2e04e83e70d9386be81acb86
CyberChild <darkpaladinknight@gmail.com>
2014-05-30 11:17:26 +0530

    Update stack_overflow.py

--- stack_overflow.py
+++ stack_overflow.py
@@ -4,7 +4,6 @@ import urllib.request
 url = 'http://59.145.203.105/'
 values = {'txtRoll' : '1305024',
           'txtEnroll' : '13139428723',
-          'btnSubmit' : 'formSubmit'
            }
 
 data = urllib.parse.urlencode(values)
