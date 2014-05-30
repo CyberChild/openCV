538bfbbf9b566910fdf3e283a46b46ca6c1cf3e6
CyberChild <darkpaladinknight@gmail.com>
2014-05-30 22:59:30 +0530

    Update stack_overflow.py

--- stack_overflow.py
+++ stack_overflow.py
@@ -4,11 +4,14 @@ import urllib.request
 url = 'http://59.145.203.105/'
 values = {'txtRoll' : '1305024',
           'txtEnroll' : '13139428723',
+          'txhSunmit'  : 'Submit'
            }
 
 data = urllib.parse.urlencode(values)
-binary_data=data.encode('ascii')
+binary_data=data.encode()
 req = urllib.request.Request(url, binary_data)
 response = urllib.request.urlopen(req)
-the_page = response.read()
-print(the_page)
+the_page = response.read().decode()
+html=open('Grade_Sheet.html','w+')
+print(the_page,file=html)
+html.close()
